c2146135c6bdda34957110da03137aaf0f5553f6
TechnicFox <technicfox@proton.me>
2024-09-09 21:37:21 +0100

    Супер апдейт додав нові емоції папки і тд

--- src/main/java/org/technicfox/emojiesFox/EmojiesFox.java
+++ src/main/java/org/technicfox/emojiesFox/EmojiesFox.java
@@ -1,20 +1,18 @@
 package org.technicfox.emojiesFox;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.technicfox.emojiesFox.handlers.CommandHandler;
 import org.technicfox.emojiesFox.util.ConfigUtil;
 
-import java.util.Arrays;
 
-
-public final class EmojiesFox extends JavaPlugin {
+public final class EmojiesFox extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+
         saveDefaultConfig();
-
-
         ConfigUtil config = new ConfigUtil(this, "config.yml");
         if (!config.getFile().exists()) {
             config.getConfig().setDefaults(getConfig().getDefaults());
@@ -22,9 +20,10 @@ public final class EmojiesFox extends JavaPlugin {
         }
 
 
-
         Bukkit.getLogger().info("Starting Emojies by TECHNICFOX");
         this.getCommand("emojis").setExecutor(new CommandHandler(this, config));
+
+
     }
 
     @Override
--- src/main/java/org/technicfox/emojiesFox/handlers/CommandHandler.java
+++ src/main/java/org/technicfox/emojiesFox/handlers/CommandHandler.java
@@ -13,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.profile.PlayerProfile;
 import org.bukkit.profile.PlayerTextures;
@@ -23,6 +24,7 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,24 +32,84 @@ import java.util.UUID;
 
 public class CommandHandler implements Listener, CommandExecutor {
     private final String invName = "Панель Емодзі";
+    private List<String> avaibleInv = new ArrayList<String>();
     private final ConfigUtil config;
 
-    public CommandHandler(EmojiesFox plugin, ConfigUtil configuration) {
+    /**
+     * Creates a new instance of the command handler.
+     *
+     * @param plugin    the plugin instance
+     * @param config    the configuration utility
+     */
+    public CommandHandler(EmojiesFox plugin, ConfigUtil config) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
-        config = configuration;
+        this.config = config;
+        for (int i = 0; i < 27; i++) {
+            try {
+                List<String> data = (List<String>) config.getConfig().getList("inventories.slot"+i);
+                Bukkit.getLogger().info(data.get(0));
+                avaibleInv.add(data.get(0));
+                avaibleInv.add(invName);
+
+            }catch (Exception ignored){}
+        }
     }
 
+    /**
+     * Opens an inventory for the given player with the given inventory name.
+     * The inventory contains all the emotes from the configuration.
+     *
+     * @param player the player to open the inventory for
+     * @param invName the name of the inventory
+     */
+    public void openInventory(Player player, String invName) {
+        Inventory inv = Bukkit.createInventory(player, 9 * 4, invName);
+        for (int i = 0; i < 27; i++) {
+            try {
+                List<String> data = (List<String>) config.getConfig().getList( invName + ".slot" + i);
+                inv.setItem(i, getHead(data.get(0), data.get(1)));
+            }catch (Exception ignored){}
+        }
+        ItemStack glass = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+        ItemMeta meta2 = glass.getItemMeta();
+        meta2.setItemName(" ");
+        glass.setItemMeta(meta2);
+        for (int i = 27; i < 35; i++) {
+            inv.setItem(i, glass);
+        }
+        ItemStack barrier = new ItemStack(Material.BARRIER);
+        ItemMeta meta = barrier.getItemMeta();
+        meta.setItemName("§cВийти");
+        barrier.setItemMeta(meta);
+        inv.setItem(35, barrier);
+        player.openInventory(inv);
+    }
+    /**
+     * Called when the player clicks in the emote inventory.
+     * Copies the emote from the clicked slot to the player's clipboard.
+     *
+     * @param event InventoryClickEvent
+     */
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (!event.getView().getTitle().equals(invName)) {
+        if (!avaibleInv.contains(event.getView().getTitle())) {
             return;
         }
-
+        if (event.getView().getTitle().equals(invName)) {
+            Player player = (Player) event.getWhoClicked();
+            int slot = event.getSlot();
+            if (slot == 35){player.closeInventory();}
+            try {
+                List<String> data = (List<String>) config.getConfig().getList("inventories.slot"+slot);
+                openInventory(player, data.get(0));
+            }catch (Exception ignored){}
+        }
         Player player = (Player) event.getWhoClicked();
         int slot = event.getSlot();
-        try {
-            List<String> data = (List<String>) config.getConfig().getList("slot"+slot);
+        if (slot == 35){player.closeInventory();}
 
+        try {
+            List<String> data = (List<String>) config.getConfig().getList(event.getView().getTitle()+".slot"+slot);
 
             Toolkit toolkit = Toolkit.getDefaultToolkit();
             Clipboard clipboard = toolkit.getSystemClipboard();
@@ -64,36 +126,64 @@ public class CommandHandler implements Listener, CommandExecutor {
 
     }
 
+    /**
+     * Called when the player uses the /emojis command.
+     * Opens an inventory with all the emotes.
+     *
+     * @param sender  the sender of the command
+     * @param command the command used
+     * @param label   the label of the command
+     * @param args    the arguments of the command
+     * @return        true if the command was successful
+     */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("Тільки гравці можуть користуватися цією командою");
-            return true;
+            return false;
         }
 
-
         Player player_who_sent_cmd = (Player) sender;
-        Inventory inv = Bukkit.createInventory(player_who_sent_cmd, 9 * 3, invName);
+        Inventory inv = Bukkit.createInventory(player_who_sent_cmd, 9 * 4, invName);
         for (int i = 0; i < 27; i++) {
             try {
-                List<String> data = (List<String>) config.getConfig().getList("slot" + i);
+                List<String> data = (List<String>) config.getConfig().getList("inventories.slot" + i);
                 inv.setItem(i, getHead(data.get(0), data.get(1)));
-            }catch (Exception ignored){
-                player_who_sent_cmd.sendMessage(ChatColor.RED + "Ойо, щось пішло не так");
-            }
+            }catch (Exception ignored){}
+        }
+
+        ItemStack barrier = new ItemStack(Material.BARRIER);
+        ItemMeta meta = barrier.getItemMeta();
+        meta.setItemName("§cВийти");
+        barrier.setItemMeta(meta);
+        inv.setItem(35, barrier);
+        ItemStack glass = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+        ItemMeta meta2 = glass.getItemMeta();
+        meta2.setItemName(" ");
+        glass.setItemMeta(meta2);
+
+        for (int i = 27; i < 35; i++) {
+            inv.setItem(i, glass);
         }
         player_who_sent_cmd.openInventory(inv);
         return true;
-
     }
 
 
+
+        /**
+         * Creates a player head item with a given name and texture URL.
+         *
+         * @param name the name to be displayed on the player head
+         * @param url the URL of the player head texture
+         * @return a new ItemStack containing the player head
+         */
     public ItemStack getHead(String name, String url) {
         UUID uuid = UUID.randomUUID();
 
         final ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
-        skullMeta.setDisplayName(name);
+        skullMeta.setItemName("§f"+name);
 
         PlayerProfile playerProfile = Bukkit.createPlayerProfile(uuid);
         PlayerTextures playerTextures = playerProfile.getTextures();
